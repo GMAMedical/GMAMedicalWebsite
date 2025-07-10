8ffe666388ad82af4944340d28a40ae5edcdfd7e
Caden Epp <ccepp2412@gmail.com>
2025-07-10 12:46:25 -0500

    more styling to home page plus added media queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,29 +12,29 @@ export default function Home() {
       <div id='banner-top-home' className='relative text-center mt-[45px]'>
         <img src="/Images/blue-cover-2.png" alt="Blue Surgical Background" className='w-full blur-sm' />
 
-        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-          <h1 className='text-[120px] font-bold font-roboto-condensed text-gma-text-white'>GMA MEDICAL</h1>
-          <h3 className='mt-2 text-lg font-roboto-condensed text-gma-text-white'>
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full'>
+          <h1 className='text-[45px] font-bold font-roboto-condensed text-gma-text-white w-full md:text-[68px] lg:text-[120px]'>GMA MEDICAL</h1>
+          <h3 className='mt-2 text-[13px] px-[5px] font-roboto-condensed text-gma-text-white lg:text-[20px]'>
             Quality service backed by quality products by companies the world trust.
           </h3>
         </div>
       </div>
 
 
-    <div id='our-mission' className='my-[80px] text-center px-4'>
-        <h2 className='text-[45px] font-roboto-condensed font-bold text-gma-text-title'>
-        Our Mission
-      </h2>
+      <div id='our-mission' className='my-[80px] text-center px-4'>
+        <h2 className='text-[38px] font-roboto-condensed font-bold text-gma-text-title lg:text-[45px]'>
+          Our Mission
+        </h2>
 
-      <p className='text-[19px] font-roboto-condensed text-gma-text-p max-w-[900px] mx-auto leading-[45px] mt-4'>
-        GMA Medical is focused on providing new and cost efficient technologies for the Surgical Services market that will improve patient outcomes, improve a facility's processes, and improve the surgeon's ability to perform procedures. Our commitment is to assist our customers to meet their objectives.
-      </p>
-    </div>
+        <p className='text-[18px] font-roboto-condensed text-gma-text-p max-w-[900px] mx-auto leading-[35px] lg:leading-[45px] mt-4'>
+          GMA Medical is focused on providing new and cost efficient technologies for the Surgical Services market that will improve patient outcomes, improve a facility's processes, and improve the surgeon's ability to perform procedures. Our commitment is to assist our customers to meet their objectives.
+        </p>
+      </div>
 
 
-      <div id='manufacturers'>
-        <h2>MANUFACTURERS</h2>
-        <h3>These companies are what makes US happen.</h3>
+      <div id='manufacturers' className='bg-gma-gray p-[55px] text-center'>
+        <h2 className='text-gma-text-white font-roboto-condensed text-[38px] lg:text-[45px]'>MANUFACTURERS</h2>
+        <h3 className='text-gma-text-white font-roboto-condensed text-[18px] px-[12px]'>These companies are what makes US happen.</h3>
         <div id='manufacturers-images'>
           {/* TODO add all static manufacturers images when Jeff sends them */}
         </div>

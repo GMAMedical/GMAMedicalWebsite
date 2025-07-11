81785e52942f8d42c2249c587735ee316020acc6
Caden Epp <ccepp2412@gmail.com>
2025-07-10 23:53:25 -0500

    styles for Products, Contact, and Login

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -4,19 +4,27 @@ export default function Contact() {
   return (
     <div>
 
-      <div id='banner-top-contact'>
-        <img src="/Images/GMA-Logo-w-text.jpg" alt="GMA Medical Logo" />
+      <div className="w-screen flex justify-center py-[55px]">
+        <img 
+          src="/Images/GMA-Logo-w-text.jpg"
+          alt="GMA Medical Logo"
+          className="w-5/6 rounded-2xl"
+        />
       </div>
 
-      <div id='form-container-contact'>
-        <div id='title-contact'>
-          <h2>CONTACT</h2>
-          <h3>Help us help you.</h3>
+      <div id='form-container-contact' className="text-center w-full">
+
+        <div id='title-contact' className="bg-gma-gray p-[55px] w-5/6 mx-auto rounded-2xl">
+          <h2 className="font-roboto-condensed text-gma-text-white text-[65px] font-bold">CONTACT</h2>
+          <h3 className="font-roboto-condensed text-gma-text-white text-[20px]">Help us help you.</h3>
         </div>
+
         <div id='contact-form'>
           {/* TODO apply contact form here */}
         </div>
+
       </div>
+
       
     </div>
   );
--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -4,14 +4,20 @@ export default function Login() {
   return (
     <div>
 
-      <div id='login-container'>
-        <div id='title-login'>
-          <h2>LOGIN</h2>
-          <h3>GMA Medical Admin Login</h3>
+      <div id='login-container' className="w-full text-center pt-[55px]">
+
+        <div id='title-login' className="bg-gma-gray p-[55px] w-5/6 mx-auto rounded-2xl">
+
+          <h2 className="font-roboto-condensed text-gma-text-white text-[65px] font-bold">LOGIN</h2>
+
+          <h3 className="font-roboto-condensed text-gma-text-white text-[20px]">GMA Medical Admin Login</h3>
+
         </div>
+
         <div id='login-form'>
           {/* TODO apply login form here */}
         </div>
+
       </div>
 
     </div>

ad4dacfc4bcaf73d4235c31e914e395a1041feda
Caden Epp <ccepp2412@gmail.com>
2025-07-12 00:30:38 -0500

    fixed some styling issues on mobile view

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -15,7 +15,7 @@ export default function Contact() {
       <div id='form-container-contact' className="text-center w-full">
 
         <div id='title-contact' className="bg-gma-gray p-[55px] w-5/6 mx-auto rounded-2xl">
-          <h2 className="font-roboto-condensed text-gma-text-white text-[65px] font-bold">CONTACT</h2>
+          <h2 className="font-roboto-condensed text-gma-text-white text-[55px] font-bold">CONTACT</h2>
           <h3 className="font-roboto-condensed text-gma-text-white text-[20px]">Help us help you.</h3>
         </div>
 

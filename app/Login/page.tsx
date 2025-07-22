8fceaa76ee54930a77d98fe289ae3f2a61f2944a
Caden Epp <ccepp2412@gmail.com>
2025-07-22 08:46:48 -0500

    trying to fix path for amplify_outputs.json

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -2,6 +2,11 @@
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+import { Amplify } from 'aws-amplify'
+import outputs from "@/amplify_outputs.json";
+
+Amplify.configure(outputs, { ssr: true })
+
 export default function Login() {
   return (
     <div>
@@ -17,9 +22,8 @@ export default function Login() {
         </div>
 
         <div id='login-form'>
-          {/* TODO apply login form here */}
           <Authenticator hideSignUp>
-            
+
           </Authenticator>
         </div>
 

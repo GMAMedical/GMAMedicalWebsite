d2d4be1decf4c97cd9d8cb34bc2ca7ac0476be80
Caden Epp <ccepp2412@gmail.com>
2025-07-23 19:04:32 -0500

    added auto page routing for admin and login pages aswell as navbar based on auth status

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,11 +1,34 @@
 'use client';
-
+import outputs from "@/amplify_outputs.json";
+import { Amplify } from 'aws-amplify'
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getCurrentUser } from 'aws-amplify/auth';
+import { useRouter } from "next/navigation";
+
+
+Amplify.configure(outputs, { ssr: true })
 
 export default function Header() {
 
+    const router = useRouter();
+
     const [menuOpen, setMenuOpen] = useState(false);
+    const [menuStatus, setMenuStatus] = useState(false);
+
+    useEffect(() => {
+        const checkUser = async () => {
+          try {
+            await getCurrentUser();
+            setMenuStatus(true);
+          } catch {
+            setMenuStatus(false);
+          }
+        };
+        checkUser();
+      }, [router]);
+
+      const menuLinks = menuStatus ? ["Products", "Contact", "Admin"] : ["Products", "Contact", "Login"];
 
     return (
         <nav className='bg-gma-blue relative px-4 py-4 flex justify-between items-center'>
@@ -21,7 +44,7 @@ export default function Header() {
 
             {/* Desktop Nav */}
             <div className='hidden md:flex items-center space-x-12 lg:space-x-24 h-full'>
-                {["Products", "Contact", "Login"].map((text) => (
+                {menuLinks.map((text) => (
                     <Link
                         key={text}
                         href={`/${text}`}
--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -2,12 +2,44 @@
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+import { useEffect, useState } from "react";
+import { getCurrentUser } from 'aws-amplify/auth';
+import { useRouter } from "next/navigation";
+
 import { Amplify } from 'aws-amplify'
 import outputs from "@/amplify_outputs.json";
+import { Hub } from 'aws-amplify/utils';
 
 Amplify.configure(outputs, { ssr: true })
 
 export default function Login() {
+
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  const checkUser = async () => {
+    try {
+      await getCurrentUser();
+      router.push("/Admin");
+    } catch {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+
+    checkUser();
+
+    const unsubscribe = Hub.listen("auth", ({ payload }) => {
+      if (payload.event === "signedIn") {
+        checkUser();
+      }
+    });
+
+    return () => unsubscribe();
+    
+  }, [router]);
+
   return (
     <div>
 
